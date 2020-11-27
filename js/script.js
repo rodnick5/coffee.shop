fd00ebb85cdee81c587ee7153809f3d86ad11579
rodnick5 <69417416+rodnick5@users.noreply.github.com>
2020-11-27 23:58:43 +0200

    slider added, back to top button fixed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,44 @@
+let mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
 function backToTop() {
     if (window.pageYOffset > 0) {
       window.scrollBy(0, -30);
       setTimeout(backToTop, 0);
     }
   }
+
+  function trackScroll(){
+    let scrollHeight = document.documentElement.scrollHeight;
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    let scrolled = scrollTop + document.documentElement.clientHeight;
+    let width = document.documentElement.clientWidth;
+    if(width > 991){
+      if(scrolled == scrollHeight){
+        goTopBtn.classList.add('back-to-top-show');
+      }
+      else{
+        goTopBtn.classList.remove('back-to-top-show');
+      }
+    } else{
+      goTopBtn.classList.remove('back-to-top-show');
+    }
+  }
   let goTopBtn = document.getElementById('back-top');
+  window.addEventListener('scroll', trackScroll);
   goTopBtn.addEventListener('click', backToTop);
-  
