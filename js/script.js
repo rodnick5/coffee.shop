ee1273911d1266a4beb6989d4071f6359e2854ae
rodnick5 <69417416+rodnick5@users.noreply.github.com>
2020-12-02 21:41:54 +0200

    adaptive complete

--- js/script.js
+++ js/script.js
@@ -42,3 +42,15 @@ function backToTop() {
   let goTopBtn = document.getElementById('back-top');
   window.addEventListener('scroll', trackScroll);
   goTopBtn.addEventListener('click', backToTop);
+
+  let menuBtn = document.getElementById('menu-burger-button');
+  menuBtn.addEventListener('click' , function(){
+    let navLinks = document.querySelectorAll('.navigation-links');
+    let nav = document.getElementById('nav-wrapper');
+    menuBtn.classList.toggle('active-menu-btn');
+    for (let i of navLinks) {
+      i.classList.toggle('nav-active-links');
+    }
+    nav.classList.toggle('active-nav-line');
+  });
+  
