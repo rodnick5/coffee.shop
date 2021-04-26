222686f5a029868409d1f4b0a9cb1860f8d15a89
rodnick5 <69417416+rodnick5@users.noreply.github.com>
2021-04-26 21:47:36 +0300

    product block structure fixed

--- js/script.js
+++ js/script.js
@@ -15,7 +15,16 @@ let mySwiper = new Swiper('.swiper-container', {
   },
 
 });
-
+let CntrBtn = document.getElementById('countryBtn');
+CntrBtn.addEventListener('click', () => {
+  document.getElementById('countryMarks').classList.toggle('country-list-active');
+  document.getElementById('imgCountry').classList.toggle('rotate180');
+});
+let TrdBtn = document.getElementById('trdBtn');
+TrdBtn.addEventListener('click' , () => {
+  document.getElementById('trdList').classList.toggle('list-trade-active');
+  document.getElementById('imgTrade').classList.toggle('rotate180');
+});
 document.getElementById('filter-btn1').addEventListener('click', fltrMenu);
 document.getElementById('filter-btn2').addEventListener('click', fltrMenu);
 
