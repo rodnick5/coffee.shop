4632bfa8d29c72aceb8009824d8713712b10a89b
rodnick5 <69417416+rodnick5@users.noreply.github.com>
2021-08-10 16:11:13 +0300

    menu grams works

--- js/script.js
+++ js/script.js
@@ -1,36 +1,3 @@
-let mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'horizontal',
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-
-let countryBtn = document.getElementById('countryBtn');
-countryBtn.addEventListener('click', function func1 (){
-  document.getElementById('countryMarks').classList.toggle('country-list-active');
-  document.getElementById('imgCountry').classList.toggle('rotate180');
-});
-let TrdBtn = document.getElementById('trdBtn');
-TrdBtn.addEventListener('click' , function func2() {
-  document.getElementById('trdList').classList.toggle('list-trade-active');
-  document.getElementById('imgTrade').classList.toggle('rotate180');
-});
-document.getElementById('filter-btn1').addEventListener('click', fltrMenu);
-document.getElementById('filter-btn2').addEventListener('click', fltrMenu);
-
-function fltrMenu() {
-  let menu = document.getElementById('filter-menu');
-  menu.classList.toggle("filter-active");
-  document.body.classList.toggle("freeze-page");
-};
-
 function backToTop() {
     if (window.pageYOffset > 0) {
       window.scrollBy(0, -30);
---
+++ js/swiper.js
@@ -0,0 +1,13 @@
+let mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
